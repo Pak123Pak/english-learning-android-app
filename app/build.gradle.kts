01034fdc7a5b9780fcd4922fde5cbc418a1e47fb
Pak123Pak <savefile6709u@gmail.com>
2025-11-13 10:35:00 +0800

    rebrand app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.englishlearningandroidapp"
         minSdk = 24
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.5.0"
+        versionCode = 7
+        versionName = "1.5.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
